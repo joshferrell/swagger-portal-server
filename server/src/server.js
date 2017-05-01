7ef264271b562755f227f65c2cd10b4bafc111dc
Joshua Ferrell <josh@joshferrell.me>
2017-04-30 19:25:00 -0700

    feat: added integration test for smoke testing postgres connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,11 @@
 import Hapi from 'hapi';
 import { dbConnect } from './db/index';
+import config from './config/index';
 
 const server = new Hapi.Server();
 server.connection({
     host: 'localhost',
-    port: 8000
+    port: config.port
 });
 
 server.route({
@@ -15,7 +16,10 @@ server.route({
 
 export const startServer = async () => {
     try {
-        await dbConnect();
+        const connection = await dbConnect();
+        if (connection === false) {
+            throw new Error('Unable to connect to database');
+        }
 
         server.start((err) => {
             if (err) {
