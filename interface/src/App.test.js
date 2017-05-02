4e32b185c0c30d8e5d9f9d34d5fecce654b9d013
Erick Fleming <efleming969@gmail.com>
2017-05-01 23:55:14 -0700

    added simple filter search component

--- interface/src/App.test.js
+++ interface/src/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it( "should render the search view by default", function() {
+  const component = renderer.create( <App /> )
+  expect( component.toJSON() ).toMatchSnapshot();
+} )
